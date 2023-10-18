165648e13993188ec9761f73932d8329a67d1121
Tmwonder1 <fabiandname@gmail.com>
2023-10-18 01:27:53 +0100

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * _puts - Prints a string followed by a new line to stdout.
+ * @str: A pointer to the string to be printed.
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+
+	}
+	write(1, "\n", 1);
+
+}
