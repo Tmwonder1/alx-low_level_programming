64faa95e42a4b77723905c0754c14010b15d255b
Tmwonder1 <fabiandname@gmail.com>
2024-04-18 13:08:21 +0100

    mi

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,36 +1,63 @@
 #include "hash_tables.h"
 
+unsigned long int hash(const char *str, unsigned long int size);
+
 /**
-* hash_table_create - Creates a hash table
-* @size: The size of the array of the hash table
+* hash_table_set - Adds an element to the hash table.
+* @ht: pointer to the hash table
+* @key: the key to add - cannot be an empty string
+* @value: the value associated with the key
 *
-* Return: A pointer to the newly created hash table, or NULL on failure
+* Return: 1 if it succeeded, 0 otherwise
 */
-hash_table_t *hash_table_create(unsigned long int size)
-{
-	hash_table_t *new_table;
-	unsigned long int i;
-
-	new_table = malloc(sizeof(hash_table_t));
-	if (new_table == NULL)
-		return (NULL);
-
-	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (new_table->array == NULL)
-	{
-		free(new_table);
-		return (NULL);
-	}
-
-
-	for (i = 0; i < size; i++)
-		new_table->array[i] = NULL;
-
-	return (new_table);
-}
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	return (0);
+	unsigned long int index;
+
+	hash_node_t *new_node;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	index = hash(key, ht->size);
+
+	/* Allocate and setup new node */
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return (0);
+	}
+
+	/* Insert node at the beginning of the linked list */
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
+
+/**
+* hash - Basic hash function to compute the index of a key
+* @str: the string to hash
+* @size: the size of the array
+*
+* Return: The computed hash
+*/
+unsigned long int hash(const char *str, unsigned long int size)
+{
+	unsigned long int hash = 5381;
+
+	int c;
+
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+	return (hash % size);
 }
